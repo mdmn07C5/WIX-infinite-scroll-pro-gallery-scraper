b865949037e8ea0ab5fa1ee276b6839f859b3e1c
mdmn07C5 <malicdan@protonmail.ch>
2023-09-26 16:48:26 -0400

    fix:     removed bs4 import

--- src/scrolling_scraper.py
+++ src/scrolling_scraper.py
@@ -4,8 +4,6 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from bs4 import BeautifulSoup, ResultSet
-
 
 
 def start_webdriver(url:str) -> webdriver:
